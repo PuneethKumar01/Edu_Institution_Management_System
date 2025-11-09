64cf3eaa3360f971da729f62fc86dbf03e480a78
puneethkumar01 <puneethkumar1405@gmail.com>
2025-11-09 08:53:29 +0530

    .

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,15 +12,17 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import environ
 import os
-from dotenv import load_dotenv
 
-# Load .env file
-load_dotenv()
-
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR: Path = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -134,10 +136,11 @@ DEFAULT_AUTO_FIELD: str = "django.db.models.BigAutoField"
 # Custom User Model
 AUTH_USER_MODEL = 'accounts.User'
 
+#email
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "puneethkumar1405@gmail.com"
-EMAIL_HOST_PASSWORD = "fuuh rjqw mbte zeil"
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
